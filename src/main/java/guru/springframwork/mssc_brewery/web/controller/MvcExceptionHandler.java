f22b2d6ad566ce04f803224e5cc344f949e62c3d
yonatanmengesha <yonatan.mengesha2017@gmail.com>
2025-06-16 16:19:26 -0400

    adding Bind Exception. closes #8

--- src/main/java/guru/springframwork/mssc_brewery/web/controller/MvcExceptionHandler.java
+++ src/main/java/guru/springframwork/mssc_brewery/web/controller/MvcExceptionHandler.java
@@ -3,6 +3,7 @@ package guru.springframwork.mssc_brewery.web.controller;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,4 +25,15 @@ public class MvcExceptionHandler {
         return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
 
     }
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<List> handleBindException(BindException ex){
+
+        return new ResponseEntity(ex.getAllErrors(), HttpStatus.BAD_REQUEST);
+    }
+
+
+
+
+
+
 }
